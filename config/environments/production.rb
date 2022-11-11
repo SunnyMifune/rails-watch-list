011cedb7c354287c01514b9ae9033a639d3efde4
sunny liu <372930@gmail.com>
2022-11-11 17:00:11 +0900

    2nd one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.active_storage.service = :cloudinary
-  
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
